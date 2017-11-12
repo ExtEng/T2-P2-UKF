44e9551c8f7bca81a67f107ec2648da7d4ade236
Clifton Pereira <pereiraclifton@gmail.com>
2017-11-12 18:24:54 -0500

    test

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -104,7 +104,7 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
   if (!is_initialized_){
     bool int_init = false;
 	//Intialize x_, P_
-	x_ << 1, 1, 1, 1, 0.0;
+	x_ << 1, 1, 1, 1, 0.3;
 
     P_ << 0.15, 0, 0, 0, 0,
         0, 0.15, 0, 0, 0,
@@ -302,9 +302,10 @@ void UKF::Prediction(double delta_t) {
   //predicted state mean
   x_.fill(0.0);
   
-  for (int i = 0; i < 2 * n_aug_ + 1; i++){
+  //for (int i = 0; i < 2 * n_aug_ + 1; i++){
         x_ = x_ + weights_(i) * Xsig_pred_.col(i);
   }
+  x_ = Xsig_pred_ * weights_;
   
   cout << "Predict Step: 5"<< endl;
   cout <<"x_:"<<x_<<endl;
